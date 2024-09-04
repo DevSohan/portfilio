fd366bac086143cb5e0ead5ee76edb7838b78df4
Mohammad Sohanur Rahman <shohandot@gmail.com>
2024-09-04 23:03:12 +0200

    update skills geo

--- components/SkillCard.tsx
+++ components/SkillCard.tsx
@@ -22,23 +22,33 @@ interface Props{
 function SkillCard({skill}:Props) {
     const imagePath = `/images/skills/${skill.image}.png`;
 
-    const background = switch(skill.experience){
+    let background:string
+    switch(skill.experience){
         case Experience.Guru:
-            "bg-gradient-to-br from-[#4CAF50] to-[#1B5E20]"
+            background = "before:bg-gradient-to-br before:from-[#F48FB1] before:to-[#d81b60] bg-gradient-to-br from-[#F48FB1] to-[#d81b60] after:bg-gradient-to-br after:from-[#F48FB1] after:to-[#d81b60]"
             break
         case Experience.Expert:
-
-        
+            background = "before:bg-gradient-to-br before:from-[#4CAF50] before:to-[#1B5E20] bg-gradient-to-br from-[#4CAF50] to-[#1B5E20] after:bg-gradient-to-br after:from-[#4CAF50] after:to-[#1B5E20]"
+            break
+        case Experience.Proficient:
+            background = "before:bg-gradient-to-br before:from-[#1976D2] before:to-[#283593] bg-gradient-to-br from-[#1976D2] to-[#283593] after:bg-gradient-to-br after:from-[#1976D2] after:to-[#283593]"
+            break
+        case Experience.Intermediate:
+            background = "before:bg-gradient-to-br before:from-[#ffc107] before:to-[#f57c00] bg-gradient-to-br from-[#ffc107] to-[#f57c00] after:bg-gradient-to-br after:from-[#ffc107] after:to-[#f57c00]"
+            break
+        case Experience.Beginner:
+            background = "before:bg-gradient-to-br before:from-[#f4511e] before:to-[#b71c1c] bg-gradient-to-br from-[#f4511e] to-[#b71c1c] after:bg-gradient-to-br after:from-[#f4511e] after:to-[#b71c1c]"
+            break  
     }
 
     return (
-        <div className='before:absolute before:m-0 before:w-24 before:h-40 before:rounded-xl before:inline-block before:top-0 before:bottom-0 before:left-0 before:right-0 before:bg-green-800 before:rotate-[60deg] before:z-0 relative m-0 w-24 h-40 rounded-xl inline-block top-0 bg-green-800 after:absolute after:m-0 after:w-24 after:h-40 after:rounded-xl after:inline-block after:top-0 after:bottom-0 after:left-0 after:right-0 after:bg-green-800 after:-rotate-[60deg] after:z-0 group transition-[display] duration-300 ease-in-out'>
+        <div className={`before:absolute before:m-0 before:w-24 before:h-40 before:rounded-xl before:inline-block before:top-0 before:bottom-0 before:left-0 before:right-0 before:rotate-[60deg] before:z-0 relative m-0 w-24 h-40 rounded-xl inline-block top-0 ${background} after:absolute after:m-0 after:w-24 after:h-40 after:rounded-xl after:inline-block after:top-0 after:bottom-0 after:left-0 after:right-0 after:-rotate-[60deg] after:z-0 group transition-[display] duration-300 ease-in-out`}>
             <div className='relative z-10 w-full h-full flex flex-col gap-y-3 justify-center items-center'>
                 <div className='w-full h-16 relative'>
                     <Image src={imagePath} width={100} height={100} alt=""  className='w-full h-full object-contain'/>
                 </div>
                 <div className=''>
-                    <h3 className='text-white font-bold text-xl antialiased'>{skill.title}</h3>
+                    <h3 className='text-white font-bold text-xl antialiased drop-shadow-md'>{skill.title}</h3>
                 </div>
             </div>
         </div>
--- components/SkillsGeo.tsx
+++ components/SkillsGeo.tsx
@@ -19,23 +19,179 @@ interface Skill{
     experience: Experience
 }
 
+interface SkillSet{
+    title: string
+    skills: Skill[]
+}
+
 
 const skills:Skill[] = [
     {
-        image: "typescript",
-        title: "TypeScript",
-        experience: Experience.Expert
-    },
-    {
-        image: "javascript",
-        title: "JavaScript",
+        image: "qgis",
+        title: "QGis",
         experience: Experience.Proficient
     },
     {
-        image: "python",
-        title: "Python",
-        experience: Experience.Beginner
-    }
+        image: "arcgis",
+        title: "ArcGIS Map",
+        experience: Experience.Proficient
+    },
+    {
+        image: "arcgis-pro",
+        title: "ArcGIS Pro",
+        experience: Experience.Intermediate
+    },
+/*     {
+        image: "typescript",
+        title: "OpenStreetsMap",
+        experience: Experience.Expert
+    }, */
+    
+
+
+]
+
+const newSkills:SkillSet[] = [
+    {
+        title: "Geo-spatial Technologies",
+        skills: [
+            {
+                image: "qgis",
+                title: "QGis",
+                experience: Experience.Proficient
+            },
+            {
+                image: "arcgis",
+                title: "ArcGIS Map",
+                experience: Experience.Proficient
+            },
+            {
+                image: "arcgis-pro",
+                title: "ArcGIS Pro",
+                experience: Experience.Intermediate
+            },
+        ]
+    },
+    {
+        title: "Modeling and Visualisation",
+        skills: [
+            {
+                image: "autocad",
+                title: "AutoCAD",
+                experience: Experience.Beginner
+            },
+            {
+                image: "revit",
+                title: "Revit",
+                experience: Experience.Beginner
+            },
+        ]
+    },
+    {
+        title: "Programming and Scripting",
+        skills: [
+            {
+                image: "python",
+                title: "Python",
+                experience: Experience.Proficient
+            },
+            {
+                image: "sql",
+                title: "SQL",
+                experience: Experience.Proficient
+            },
+            {
+                image: "typescript",
+                title: "TypeScript",
+                experience: Experience.Proficient
+            },
+            {
+                image: "javascript",
+                title: "JavaScript",
+                experience: Experience.Proficient
+            },
+            {
+                image: "nodejs",
+                title: "NodeJs",
+                experience: Experience.Beginner
+            },
+            {
+                image: "react",
+                title: "React",
+                experience: Experience.Expert
+            },
+            {
+                image: "jquery",
+                title: "JQuery",
+                experience: Experience.Expert
+            },
+            {
+                image: "flask",
+                title: "Flask",
+                experience: Experience.Intermediate
+            },
+            {
+                image: "django",
+                title: "DJango",
+                experience: Experience.Intermediate
+            },
+            {
+                image: "rest-api",
+                title: "Rest API",
+                experience: Experience.Expert
+            },
+        ]
+    },
+    {
+        title: "Databases",
+        skills: [
+            {
+                image: "postgres",
+                title: "PostgreSQL",
+                experience: Experience.Proficient
+            },
+            {
+                image: "postgis",
+                title: "PostGIS",
+                experience: Experience.Beginner
+            },
+            {
+                image: "mongodb",
+                title: "MongoDB",
+                experience: Experience.Intermediate
+            },
+            {
+                image: "mysql",
+                title: "MySQL",
+                experience: Experience.Expert
+            },
+        ]
+    },
+    {
+        title: "DevOps",
+        skills: [
+            {
+                image: "git",
+                title: "Git",
+                experience: Experience.Proficient
+            },
+            {
+                image: "github",
+                title: "Github",
+                experience: Experience.Proficient
+            },
+            {
+                image: "gitlab",
+                title: "GitLab",
+                experience: Experience.Intermediate
+            },
+            {
+                image: "docker",
+                title: "Docker",
+                experience: Experience.Proficient
+            },
+        ]
+    },
 ]
 
 function SkillsGeo() {
@@ -45,21 +201,31 @@ function SkillsGeo() {
                 <h1 className='text-xl font-bold uppercase text-[#55e6a5] mb-4'>
                     Skills
                 </h1>
-                <h2 className='text-lg md:text-3xl lg:text-5xl leading-8 md:leading-12 capitalize mb-12 font-bold text-white'>
+                <h2 className='text-lg md:text-3xl lg:text-5xl leading-8 md:leading-12 capitalize font-bold text-white'>
                     Transforming <span className='text-yellow-400'>Visions</span>
                 </h2>
             </div>
-            <div className='text-center pt-5 md:pt-10 mx-auto lg:max-w-screen-xl md:max-w-screen-lg'>
-                <div className='flex flex-row flex-wrap gap-x-28 gap-y-10 justify-center align-center'>
-                    {
-                        skills.map((skill, key) => {
-                            return(
-                                <SkillCard key={key} skill={skill} />
-                            )
-                        })
-                    }
+            <div className='text-center mx-auto mb-3 md:mb-10 lg:mb-16 lg:max-w-screen-xl md:max-w-screen-lg'>
+                {
+                    newSkills.map((skillset, key) => {
+                        return(
+                            <div key={key}>
+                                <h2 className='text-2xl font-bold antialiased text-white py-6 md:pb-10 md:pt-16'>{skillset.title}</h2>
+                                <div className='flex flex-row flex-wrap gap-x-28 gap-y-10 justify-center align-center'>
+                                    {
+                                        skillset.skills.map((skill, key) => {
+                                            return(
+                                                <SkillCard key={key} skill={skill} />
+                                            )
+                                        })
+                                    }
 
-                </div>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+                
             </div>
         </div>
     )
